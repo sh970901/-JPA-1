a643399eac698dc7ca623a17f35a69be8d3064d8
sh970901 <aaddss639@naver.com>
2023-01-21 17:01:23 +0900

    ManyToMany 관계는 실무에서는 중간테이블을 직접 만들어서 분리

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,8 +1,12 @@
 package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) //한 테이블에 다 때려박음
@@ -16,6 +20,9 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories  = new ArrayList<>();
 
 
 }
